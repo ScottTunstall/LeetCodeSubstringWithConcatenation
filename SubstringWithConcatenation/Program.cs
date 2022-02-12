033f708f4b76927fea7b227cdb2e31028fcea587
Scott Tunstall <scott.tunstall@ntlworld.com>
2022-02-12 14:57:12 +0000

    OK, version 1, using a variation of the permutations calculator I learned about earlier.

---
+++ SubstringWithConcatenation/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using SubstringWithConcatenation;
+
+var x = new Solution();
+//var positions = x.FindSubstring("barfoothefoobarman", new string[] { "foo","bar" });
+
+var positions = x.FindSubstring("wordgoodgoodgoodbestword", new string[] {"word", "good", "best", "good"});
+
+Console.WriteLine();
---
+++ SubstringWithConcatenation/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstringWithConcatenation
+{
+    public class Solution
+    {
+        private UniquePermutationsCalculator _perms = new();
+
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            var perms = _perms.GetUniquePermutations(words);
+
+            var positionsFoundAt = new List<int>();
+
+            foreach (var perm in perms)
+            {
+                var pos = s.IndexOf(perm);
+                if (pos>-1)
+                    positionsFoundAt.Add(pos);
+            }
+
+            return positionsFoundAt;
+        }
+    }
+}
