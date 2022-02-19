d55db467270248582f3caeff37caeecb9430f6c8
Scott Tunstall <scott.tunstall@ntlworld.com>
2022-02-19 20:40:00 +0000

    Some more tweaks.
    Runtime: 1438 ms, faster than 13.04% of C# online submissions for Substring with Concatenation of All Words.
    
    Memory Usage: 46.3 MB, less than 42.51% of C# online submissions for Substring with Concatenation of All Words.

--- SubstringWithConcatenation/Solution.cs
+++ SubstringWithConcatenation/Solution.cs
@@ -13,24 +13,21 @@ namespace SubstringWithConcatenation
             var stringLen = s.Length;
             var foundIndices = new List<int>();
 
-            Dictionary<string, short> usedWords = new();
-            HashSet<char> firstLetterOfEachWord = new();
+            Dictionary<string, short> wordToCountMap = new();
 
             foreach (var word in words)
             {
-                usedWords[word] = 0;
-                firstLetterOfEachWord.Add(word[0]);
+                wordToCountMap[word] = 0;
             }
 
             foreach (var word in words)
             {
-                // There may be a duplicate word in the word list, so count how many instances
-                // of each word there are.
-                usedWords[word] += 1;
+                // As words may be duplicated, count how many instances of each word there are.
+                wordToCountMap[word] += 1;
             }
             
             // Clone dictionary
-            Dictionary<string, short> reloadCache = new(usedWords);
+            Dictionary<string, short> reloadCache = new(wordToCountMap);
 
             var wordLen = words[0].Length;
             var numberOfWords = words.Length;
@@ -39,45 +36,46 @@ namespace SubstringWithConcatenation
 
             while (left+wordLen <= stringLen)
             {
-                var right = left;
-
                 // Do we have any words beginning with this letter?
-                var ch = s[left];
-                if (!firstLetterOfEachWord.Contains(ch))
-                {
-                    // No, so don't waste time with substring call. Bump "left" to look at next char in s.
-                    left++;
-                    continue;
-                }
-                
                 var countOfWords = 0;
-                
-                var readWord = s.Substring(right, wordLen);
-                
-                while (usedWords.TryGetValue(readWord, out var remaining) && remaining > 0)
+
+                bool countDictMutated = false;
+                var right = left;
+                while(true)
                 {
-                    countOfWords++;
-                    usedWords[readWord]-=1; 
-                    right += wordLen;
-                    if (right+wordLen > stringLen)
+                    // Hmm, would computing a hash value rather than using substring speed this up?
+                    var readWord = s.Substring(right, wordLen);
+                    if (!wordToCountMap.TryGetValue(readWord, out var remaining) || remaining <= 0)
+                    {
                         break;
+                    }
 
-                    readWord = s.Substring(right, wordLen);
+                    countOfWords++;
+
+                    // If all the words in the dictionary have been used (their counts are 0),
+                    // then record the start index of the sequence 
+                    if (countOfWords == numberOfWords)
+                    {
+                        foundIndices.Add(left);
+                        break;
+                    }
+
+                    wordToCountMap[readWord] -= 1;
+                    countDictMutated = true;
+                    right += wordLen;
+                    if (right + wordLen > stringLen)
+                        break;
                 }
-
-                // If all the words in the dictionary have been used, then record the start index of the sequence 
-                if (countOfWords == numberOfWords)
-                {
-                    foundIndices.Add(left);
-                }
-
 
                 // Copy reloadCache dictionary to useWords, "reloading" it for re-use
-                foreach (var (key, value) in reloadCache)
+                if (countDictMutated)
                 {
-                    usedWords[key] = value;
+                    foreach (var (key, value) in reloadCache)
+                    {
+                        wordToCountMap[key] = value;
+                    }
                 }
-                
+
                 left++;
             }
 
