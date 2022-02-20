ff0a8d113a82dab5e92dca49bb6548051e577f79
Scott Tunstall <scott.tunstall@ntlworld.com>
2022-02-20 14:44:19 +0000

    Added plenty of comments for people to understand the algorithm.

--- SubstringWithConcatenation/Solution.cs
+++ SubstringWithConcatenation/Solution.cs
@@ -5,56 +5,87 @@
         public IList<int> FindSubstring(string s, string[] words)
         {
             var stringLen = s.Length;
+            var wordLen = words[0].Length;          // Note: each word is the same length
+            var numberOfWords = words.Length;
             var foundIndices = new List<int>();
 
-            Dictionary<string, short> wordToCountMap = new();
+            // Dictionary to count how many instances of a given word there are.
+            Dictionary<string, short> wordCountMap = new();
+
+            HashSet<char> firstLetterOfEachWordSet = new();
 
             foreach (var word in words)
             {
-                wordToCountMap[word] = 0;
+                // Initialise each entry in dictionary
+                wordCountMap[word] = 0;
+                firstLetterOfEachWordSet.Add(word[0]);
             }
 
+            //count how many instances of each word there are.
+            // For example, if the words[] array contains "good" twice then 
+            // wordCountMap["good"] will be set to 2.
             foreach (var word in words)
             {
-                // As words may be duplicated, count how many instances of each word there are.
-                wordToCountMap[word] += 1;
+                wordCountMap[word] += 1;
             }
             
-            // Clone dictionary
-            Dictionary<string, short> reloadCache = new(wordToCountMap);
+            // We've now counted how many times each word appears in wordCountMap.
+            // Clone wordCountMap dictionary into wordCountCache.
+            // We use this cache to *quickly* reset wordCountMap after successfully matching 1 or more words - *even if we don't match all words*.
+            // This is *much* faster than rebuilding the wordCountMap dictionary from scratch (like we did above) as
+            // the words[] array might have hundreds of words to process and map.
+            // Each word = CPU time to process = takes longer = Leetcode timeout 
+            Dictionary<string, short> wordCountCache = new(wordCountMap);
+            
 
-            var wordLen = words[0].Length;
-            var numberOfWords = words.Length;
-
+            // Left points to the start index of a possible word sequence in string s.
             var left = 0;
 
             while (left+wordLen <= stringLen)
             {
-                // Count how many words out of the words[] array we've used. 
-                // When countOfWords == numberOfWords, we've got a successful sequence
-                var countOfWords = 0;
+                // Check to see if the character at s[left] is the first letter of a word in words[].
+                // If not, then we know left isn't pointing at the start of a word we want to match,
+                // so bump left to next character in s.
+                if (!firstLetterOfEachWordSet.Contains(s[left]))
+                {
+                    left++;
+                    continue;
+                }
 
+                // OK, left *might* point at the start of a word we can match.
+                // Left might even point at the start of a word sequence!
+                // In this case, we need to keep count of how many words out of the words[] array we've matched. 
+                // When countOfWords == numberOfWords, we've successfully matched all the words in the words[] array.
+                // We then record "left" as a match position.
+                var matchedWordCount = 0;
+
+                // Flag that determines if we found any words and wordCountMap was mutated (updated)
                 bool countDictMutated = false;
+
+                // "right" is going to be used to scan ahead for words in the string, starting from position "left"
+                // so think of left..right defining a window in substring s to check for a sequence of words.
                 var right = left;
                 do
                 {
-                    // Hmm, would computing a hash value rather than using substring speed this up?
                     var readWord = s.Substring(right, wordLen);
-                    if (!wordToCountMap.TryGetValue(readWord, out var remaining) || remaining <= 0)
+
+                    // If our word count map doesn't contain the word OR the associated word count is 0,
+                    // then exit this scan ahead loop. 
+                    if (!wordCountMap.TryGetValue(readWord, out var remaining) || remaining <= 0)
                     {
                         break;
                     }
 
-                    // Decrement number of times we're allowed to use this word again, on this pass
-                    wordToCountMap[readWord] -= 1;
+                    // Decrement number of times we're allowed to use this word again, on this pass.
+                    wordCountMap[readWord] -= 1;
 
                     countDictMutated = true;
 
-                    countOfWords++;
+                    matchedWordCount++;
 
-                    // If all the words in the dictionary have been used (their counts are 0),
-                    // then record the start index of the sequence 
-                    if (countOfWords == numberOfWords)
+                    // If all the words have been matched (their counts are 0),
+                    // then record the start index of the word sequence 
+                    if (matchedWordCount == numberOfWords)
                     {
                         foundIndices.Add(left);
                         break;
@@ -63,12 +94,14 @@
                     right += wordLen;
                 } while (right + wordLen <= stringLen);
 
-                // Copy reloadCache dictionary to useWords, "reloading" it for re-use
+                // If wordCountMap was mutated in the while loop, then it needs "reloaded" 
+                // That is to say, the word counts need reset to what they were just before entering the outer while loop.
+                // Use the cache to reload the WordCountMap.
                 if (countDictMutated)
                 {
-                    foreach (var (key, value) in reloadCache)
+                    foreach (var (key, value) in wordCountCache)
                     {
-                        wordToCountMap[key] = value;
+                        wordCountMap[key] = value;
                     }
                 }
 
